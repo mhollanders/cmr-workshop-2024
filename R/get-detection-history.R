f15725a37fb89e60a4a96d3358b7929d298acb13
Matthijs Hollanders <matthijs.hollanders@gmail.com>
2025-02-06 11:46:15 +0800

    added Jolly-Seber detection histories

--- R/get-detection-history.R
+++ R/get-detection-history.R
@@ -1,43 +1,72 @@
-library(tidyverse)
 get_dh <- function(detections, 
                    dates, 
                    date = "date", 
-                   individual = "individual", 
+                   id = "id", 
+                   jolly_seber = 0, 
+                   K = 500, 
                    covariates = NULL) {
+  
+  stopifnot(
+    "date and/or id labels not found in `detections`." = 
+      all(c(date, id) %in% colnames(detections))
+  )
+  
   # create survey column
   dates <- dates |> 
-    mutate(survey = factor(all_of(date)) |> as.numeric())
+    mutate(survey = factor(!!sym(date)) |> as.numeric())
   
   # join datasets
   dat <- detections |> 
-    select(all_of(c(date, individual, covariates))) |> 
+    select(contains(c(date, id, covariates))) |> 
     left_join(dates, by = date) |> 
     drop_na() |> 
-    mutate(ind = factor(get(individual))) |> 
-    arrange(ind)
+    mutate(individual = factor(!!sym(id))) |> 
+    arrange(individual)
   
   # get metadata
-  I <- nlevels(dat$ind)
+  I <- nlevels(dat$individual)
+  J <- max(dat$survey)
   first_last <- dat |> 
     summarise(first = min(survey), 
               last = max(survey),
-              .by = ind)
+              .by = individual)
   f <- first_last$first
   l <- first_last$last
+  tau <- dates |> 
+    mutate(tau = difftime(date, lag(date), units = "weeks")) |> 
+    pull(tau) |> 
+    as.numeric()
+  tau[1] <- 1  # for JS
   
   # fill detection history
   y <- matrix(0, I, J)
   for (i in 1:I) {
-    dat_i <- filter(dat, as.numeric(ind) == i)
+    dat_i <- filter(dat, as.numeric(individual) == i)
     for (j in f[i]:J) {
       y[i, j] <- dat_i |> 
         filter(survey == j) |>
         nrow()
     }
   }
-  list(I = I,
-       J = J, 
-       f = f, 
-       l = l,
-       y = y)
+  
+  # return Jolly-Seber data
+  if (jolly_seber) {
+    list(I = I,
+         J = J, 
+         K = K, 
+         f = f + 1, 
+         l = l + 1, 
+         tau = tau / 52,  # yearly unit by default
+         y = cbind(rep(1, I), y + 1) |> 
+           rbind(matrix(1, K, J + 1)))
+    
+    # return Cormack-Jolly-Seber data
+  } else {
+    list(I = I,
+         J = J, 
+         f = f, 
+         l = l,
+         tau = tau[-1] / 52,  # yearly unit by default
+         y = y + 1)
+  }
 }
